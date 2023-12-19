8b899510205740ee34f74537e6833f17a76f2afb
Mohamed Amine Merdessi <hamamerdessi00@gmail.com>
2023-12-19 14:05:33 +0100

    Added new Files

---
+++ server/database/User.js
@@ -0,0 +1,33 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./index.js');
+
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  address: {
+    type: DataTypes.STRING,
+  },
+  firstName: {
+    type: DataTypes.STRING,
+  },
+  lastName: {
+    type: DataTypes.STRING,
+  },
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: 'user',
+  },
+});
+
+module.exports = User;
