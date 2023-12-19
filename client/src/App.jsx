e6acd092d28ddda8c8963b2deaf382b3d96b9128
salimhanchaoui <s.hanchaoui@yahoo.fr>
2023-12-19 17:48:10 +0100

    front starter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from './components/Nav.jsx'
+import  Sign  from './components/Sign.jsx'
+import Header from './components/header.jsx'
+import Footer from './components/Footer.jsx'
+import Home from './components/Home.jsx'
 function App() {
   return (
-    <div>app</div>
+    <BrowserRouter>
+          <Nav />
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+  </BrowserRouter>
   )
 }
 
