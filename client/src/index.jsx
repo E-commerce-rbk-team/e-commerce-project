a049ff42eb17f8580e6e12439b718ed6dfd76b91
Tasn1me <tassououertani123@gmail.com>
2023-12-20 09:07:10 +0100

    made the header and the cart

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-
+import ReactDOM from "react-dom";
+import Cart from "./components/Cart.jsx"
 const App = () => {
+    return(<Cart/>)
+    
 }
-
 ReactDOM.render(<App />, document.getElementById('App'))
--- server/database/index.js
+++ server/database/index.js
@@ -1,18 +1,18 @@
-// const mysql = require('mysql2');
-// const connection = mysql.createConnection({
-//   host     : 'localhost',
-//   user     : 'root',
-//   password : 'root',
-//   database : 'ecommerce'
-// })
-// connection.connect((err) => {
-//   if (err) {
-//     console.error('Error connecting to database:', err)
-//     return
-//   }
-//   console.log('Database connected!')
-// })
-// module.exports = connection
+const mysql = require('mysql2');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Faouzia Chebbi1',
+  database : 'ecommerce'
+})
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to database:', err)
+    return
+  }
+  console.log('Database connected!')
+})
+module.exports = connection
 
 const { Sequelize } = require('sequelize');
 
