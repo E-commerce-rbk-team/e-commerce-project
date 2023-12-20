73860b625117450e98a71a1a2db5b9f11732926c
Tasn1me <tassououertani123@gmail.com>
2023-12-20 16:16:15 +0100

    add category

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,13 @@ import Product from './components/Product.jsx';
 import Home from './components/Home.jsx'
 import WhishList from './components/WhishList.jsx';
 import Cart from "./components/Cart.jsx"
+import Categories from "./components/Categories.jsx"
 function App() {
 
     return (
       <BrowserRouter>
     <Routes>
-      <Route path="/" element={<SignUp/>} />
+      <Route path="/" element={<Categories/>} />
       <Route path="/wishlist" element={<WhishList />} />
       <Route path="/sign" element={<SignUp />} />
       <Route path="/cart" element={<Cart/>} />
