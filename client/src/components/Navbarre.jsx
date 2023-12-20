016a00536fbcf3e5c025e29a29cecc0880070e60
salimhanchaoui <s.hanchaoui@yahoo.fr>
2023-12-20 22:33:24 +0100

    fixes

--- client/src/components/Navbarre.jsx
+++ client/src/components/Navbarre.jsx
@@ -6,7 +6,7 @@ function Head() {
   return (
     <header>
         <nav className="navbar">
-          <img className="logo" src="https://media.discordapp.net/attachments/1173529999295381524/1187087546820333608/image.png?ex=65959c6e&is=6583276e&hm=df9e9f1513c9b52dba719168ad71a4655e6c4eab87cd9f17edc9b97a54b0aee6&=&quality=lossless&width=375&height=248" alt="Logo" className="logo"/>
+          <img className="logo" src="https://media.discordapp.net/attachments/1173529999295381524/1187087546820333608/image.png?ex=65959c6e&is=6583276e&hm=df9e9f1513c9b52dba719168ad71a4655e6c4eab87cd9f17edc9b97a54b0aee6&=&quality=lossless&width=375&height=248" alt="Logo"/>
           <div className="nav-links">
             <NavLink to={`/`} className="nav-link">Home</NavLink>
             <NavLink to={'/Contact'} className="nav-link">Contact</NavLink>
