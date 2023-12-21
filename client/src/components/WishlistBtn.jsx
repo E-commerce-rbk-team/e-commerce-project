c92e015b06ca3e93486d3d5df911f5d85d0f0937
Mohamed Amine Merdessi <hamamerdessi00@gmail.com>
2023-12-21 15:03:58 +0100

    added new things
    added cartBtn-Color-DeliveryOptions-ProductDetails-Quantity-Size-WishListBtn

---
+++ client/src/components/WishlistBtn.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { Box, IconButton } from '@mui/material';
+import { Favorite } from '@mui/icons-material';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
+
+const WishlistButton = () => {
+  const [showAlert, setShowAlert] = useState(false);
+
+  const handleWishlistClick = () => {
+    setShowAlert(true);
+  };
+
+  const handleCloseAlert = () => {
+    setShowAlert(false);
+  };
+
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'inline-block',
+          backgroundColor: 'transparent',
+          borderRadius: '10%',
+          padding: '1px',
+          position: 'relative',
+          top: -395,
+          right: -800,
+          contentAlign: 'center',
+          '&:hover': {
+            backgroundColor: '#E0E0E0',
+          },
+        }}
+      >
+        <IconButton
+          aria-label="Add to Wishlist"
+          color="secondary"
+          sx={{
+            color: '#FF5722',
+            
+          }}
+          onClick={handleWishlistClick}
+        >
+          <Favorite />
+        </IconButton>
+      </Box>
+      <Stack sx={{ width: '300px', position: 'relative', top: '-655px', right: '-1000px', zIndex: 9999 }}>
+        {showAlert && (
+          <Alert severity="success" onClose={handleCloseAlert}>
+            Item added to Wishlist!
+          </Alert>
+        )}
+      </Stack>
+    </>
+  );
+};
+
+export default WishlistButton;
