816ae01c0ceb140ad84e799340ec47b6178ae13b
Mohamed Amine Merdessi <hamamerdessi00@gmail.com>
2023-12-21 11:25:01 +0100

    added material ui
    added material ui and updated some stuff

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter , Routes, Route} from 'react-router-dom';
 import ReactDOM from 'react-dom'
 import SignUp from './components/Sign.jsx'
 import Product from './components/Product.jsx';
-import Home from './components/Home.jsx'
+import Home from './components/ProductDetails.jsx'
 import WhishList from './components/WhishList.jsx';
 import Cart from "./components/Cart.jsx"
 import Contact from './components/Contact.jsx';
