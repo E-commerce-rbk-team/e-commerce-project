ff697db7533225a92dfb4b415b0c2c6867f562a9
Mohamed Amine Merdessi <hamamerdessi00@gmail.com>
2023-12-25 15:04:45 +0100

    deleted all

--- server/services/TokenServices.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-const secretKey = 'samotame'; // Replace with your secret key
-
-function generateToken(userId) {
-  const expiresIn = 60 * 60 * 24; // Token expiration time (e.g., 24 hours)
-  return jwt.sign({ userId }, secretKey, { expiresIn });
-}
-
-module.exports = { generateToken };
